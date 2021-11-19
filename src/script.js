d3503215fb3af466522d6d28c2adc0428c5508a3
simonproudfoot <simonproudfoot1980@gmail.com>
2021-11-19 10:20:34 +0200

    set object points

--- src/script.js
+++ src/script.js
@@ -10,56 +10,101 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 // Object 1
-const geometry1 = new THREE.BoxGeometry(0.3, 1, 0.3)
+const geometry1 = new THREE.BoxGeometry(0.1, 0.1, 0.1)
 const material1 = new THREE.MeshStandardMaterial({ color: 0xfc038c })
 const mesh1 = new THREE.Mesh(geometry1, material1)
 mesh1.name = 'box1'
 mesh1.position.set(17.360, 0, 25.860)
-mesh1.scale.set(5.810, 15.190, 5.000)
+mesh1.scale.set(5.000, 5.000, 5.000)
 scene.add(mesh1)
 // Object 2
-const geometry2 = new THREE.BoxGeometry(0.3, 1, 0.3)
+const geometry2 = new THREE.BoxGeometry(0.1, 0.1, 0.1)
 const material2 = new THREE.MeshStandardMaterial({ color: 0xfcba03 })
 const mesh2 = new THREE.Mesh(geometry2, material2)
 mesh2.name = 'box2'
 mesh2.position.set(-72.820, 0, 25.86)
-mesh2.scale.set(5.810, 15.190, 5.000)
+mesh2.scale.set(5.000, 5.000, 5.000)
 scene.add(mesh2)
 // Object 3
-const geometry3 = new THREE.BoxGeometry(0.3, 1, 0.3)
+const geometry3 = new THREE.BoxGeometry(0.1, 0.1, 0.1)
 const material3 = new THREE.MeshStandardMaterial({ color: 0x7703fc })
 const mesh3 = new THREE.Mesh(geometry3, material3)
 mesh3.name = 'box3'
-mesh3.scale.set(5.810, 15.190, 5.000)
+mesh3.scale.set(5.000, 5.000, 5.000)
 mesh3.position.set(-72.820, 0, -109.540)
 scene.add(mesh3)
 // Object 4
-const geometry4 = new THREE.BoxGeometry(0.3, 1, 0.3)
+const geometry4 = new THREE.BoxGeometry(0.1, 0.1, 0.1)
 const material4 = new THREE.MeshStandardMaterial({ color: 0x1aff00 })
 const mesh4 = new THREE.Mesh(geometry4, material4)
 mesh4.name = 'box4'
 mesh4.position.set(17.360, 0, -109.540)
-mesh4.scale.set(5.810, 15.190, 5.000)
+mesh4.scale.set(5.000, 5.000, 5.000)
 scene.add(mesh4)
 
 // Object 5
-const geometry5 = new THREE.BoxGeometry(0.3, 1, 0.3)
+const geometry5 = new THREE.BoxGeometry(0.1, 0.1, 0.1)
 const material5 = new THREE.MeshStandardMaterial({ color: 0x1aff00 })
 const mesh5 = new THREE.Mesh(geometry5, material5)
 mesh5.name = 'box5'
 mesh5.position.set(17.360, 0, -37.480)
-mesh5.scale.set(5.810, 15.190, 5.000)
+mesh5.scale.set(5.000, 5.000, 5.000)
 scene.add(mesh5)
 
+
+
 // LOOK ATS
-const geometry6 = new THREE.BoxGeometry(0.3, 1, 0.3)
-const material6 = new THREE.MeshStandardMaterial({ color: 0xff0055 })
-const mesh6 = new THREE.Mesh(geometry6, material6)
-mesh6.name = 'lookAt5'
-mesh6.position.set(0.260, 1.610, -86.610)
-mesh6.scale.set(20.000, 3.000, 17.240)
-mesh6.material.wireframe = true
-scene.add(mesh6)
+const geometry10 = new THREE.BoxGeometry(0.3, 1, 0.3)
+const material10 = new THREE.MeshStandardMaterial({ color: 0xff0055 })
+const mesh10 = new THREE.Mesh(geometry10, material10)
+mesh10.name = 'lookAt1'
+mesh10.position.set(-0.070, 2.070, 7.500)
+mesh10.scale.set(20.000, 3.000, 17.240)
+mesh10.material.wireframe = true
+scene.add(mesh10)
+
+
+const geometry7 = new THREE.BoxGeometry(0.3, 1, 0.3)
+const material7 = new THREE.MeshStandardMaterial({ color: 0xff0055 })
+const mesh7 = new THREE.Mesh(geometry7, material7)
+mesh7.name = 'lookAt2'
+mesh7.position.set(-63.670, 1.300, 5.110)
+mesh7.scale.set(20.000, 3.000, 17.240)
+mesh7.material.wireframe = true
+scene.add(mesh7)
+
+const geometry8 = new THREE.BoxGeometry(0.3, 1, 0.3)
+const material8 = new THREE.MeshStandardMaterial({ color: 0xff0055 })
+const mesh8 = new THREE.Mesh(geometry8, material8)
+mesh8.name = 'lookAt3'
+mesh8.position.set(-61.620, 1.300, -87.900)
+mesh8.scale.set(20.000, 3.000, 17.240)
+mesh8.material.wireframe = true
+scene.add(mesh8)
+
+
+const geometry9 = new THREE.BoxGeometry(0.3, 1, 0.3)
+const material9 = new THREE.MeshStandardMaterial({ color: 0xff0055 })
+const mesh9 = new THREE.Mesh(geometry9, material9)
+mesh9.name = 'lookAt4'
+mesh9.position.set(0.260, 1.610, -86.610)
+mesh9.scale.set(20.000, 3.000, 17.240)
+mesh9.material.wireframe = true
+scene.add(mesh9)
+
+
+if (window.location.hash) {
+    const geometryTest = new THREE.BoxGeometry(0.3, 1, 0.3)
+    const materialTest = new THREE.MeshStandardMaterial({ color: 0xff0055 })
+    const meshTest = new THREE.Mesh(geometry9, material9)
+    meshTest.position.set(0.260, 1.610, -86.610)
+    meshTest.scale.set(20.000, 3.000, 17.240)
+    meshTest.material.wireframe = true
+    meshTest.name = 'test cam'
+    scene.add(meshTest)
+}
+
+
 
 
 // box sizes
@@ -96,18 +141,17 @@ loader.load(
 // lights
 var hemiLight = new THREE.HemisphereLight(0xffeeb1, 0x080820, 2.5);
 scene.add(hemiLight);
-var light = new THREE.SpotLight(0xffa95c, 4);
-light.position.set(-50, 50, 50);
-light.castShadow = true;
+const light = new THREE.AmbientLight(0x404040); // soft white light
 scene.add(light);
-light.shadow.bias = -0.0001;
-light.shadow.mapSize.width = 1024 * 4;
-light.shadow.mapSize.height = 1024 * 4;
+
 // Camera
 const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 1, 1000);
-camera.position.z = 5
-camera.position.y = 1
 scene.add(camera)
+// const helper = new THREE.CameraHelper(camera);
+// scene.add(helper);
+
+
+
 //Create a closed wavey loop
 const curve = new THREE.CatmullRomCurve3([
     new THREE.Vector3(mesh1.position.x, 0, mesh1.position.z),
@@ -115,8 +159,6 @@ const curve = new THREE.CatmullRomCurve3([
     new THREE.Vector3(mesh3.position.x, 0, mesh3.position.z),
     new THREE.Vector3(mesh4.position.x, 0, mesh4.position.z),
     new THREE.Vector3(mesh5.position.x, 0, mesh5.position.z),
-    //new THREE.Vector3(mesh6.position.x, 0, mesh6.position.z),
-    //new THREE.Vector3( mesh1.position.x, 0, mesh1.position.z ),
 ]);
 curve.closed = true;
 const points = curve.getPoints(50);
@@ -137,7 +179,6 @@ renderer.setSize(sizes.width, sizes.height)
 renderer.shadowMap.enabled = true;
 renderer.physicallyCorrectLights = true
 
-
 var classname = document.getElementsByClassName("goTO");
 Array.from(classname).forEach((el) => {
     el.addEventListener("click", changeObject);
@@ -157,8 +198,10 @@ function lookAtObject(obj) {
     var endRotation = new THREE.Euler().copy(camera.rotation);
     camera.rotation.copy(startRotation);
     // Tween
-    gsap.to(camera.rotation, { x: endRotation.x, y: endRotation.y, z: endRotation.z, duration: 3 }).then(() => {
+    gsap.to(camera.rotation, { x: endRotation.x, y: endRotation.y, z: endRotation.z, duration: 1 }).then(() => {
         playing = false
+        lookAtMesh = ''
+
     })
 }
 
@@ -173,21 +216,35 @@ var pos = 0
 var playing = false
 var camPos = new THREE.Vector3(0, 0, 0); // Holds current camera position
 var pos = curve.getPointAt((t + 0.001) % 1, p2);
+var pause = false
 
+if (scene.getObjectByName('lookAt1')) {
+    camera.lookAt(scene.getObjectByName('lookAt1'))
+}
+
+camera.position.set(mesh1.position.x, mesh1.position.y, mesh1.position.z)
 
 const animate = () => {
-    if (playing) {
 
-        if (camera.position.distanceTo(curve.points[selectSpot]) < 20) {
-            lookAtObject(scene.getObjectByName(lookAtMesh))
-        } else {
-            t += 0.003;
-            curve.getPointAt(t % 1, p1);
-            curve.getPointAt((t + 0.001) % 1, p2);
-            lookAt.copy(p2).sub(p1).applyAxisAngle(axis, -Math.PI * 0.5).add(p1); // look at the point 90 deg from the path
-            camera.position.copy(p1);
-            camera.lookAt(lookAt);
-            camPos = lookAt
+    if (playing) {
+        if (!window.location.hash) {
+            if (!window.location.hash) {
+                if (camera.position.distanceTo(curve.points[selectSpot]) < 2) {
+                    if (scene.getObjectByName(lookAtMesh)) {
+                        lookAtObject(scene.getObjectByName(lookAtMesh))
+                    }
+                } else {
+                    t += 0.003;
+                    curve.getPointAt(t % 1, p1);
+                    curve.getPointAt((t + 0.001) % 1, p2);
+                    lookAt.copy(p2).sub(p1).applyAxisAngle(axis, -Math.PI * 0.5).add(p1); // look at the point 90 deg from the path
+                    camera.position.copy(p1);
+                    camera.lookAt(lookAt);
+                    camPos = lookAt
+                }
+            }
+        }else{
+            
         }
     }
     requestAnimationFrame(animate);
