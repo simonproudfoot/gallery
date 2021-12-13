19b88c0c4fb23ac8d7f916faedbbdac5a0bcc049
simonproudfoot <simonproudfoot1980@gmail.com>
2021-12-13 17:31:13 +0000

    fixed issue with item indexes

--- src/script.js
+++ src/script.js
@@ -9,7 +9,7 @@ const axios = require('axios').default;
 const loader = new GLTFLoader();
 import woodenFloor from './images/Wood_Floor_006_COLOR.jpg'
 import woodenFloorBump from './images/Wood_Floor_006_DISP.png'
-
+import { RenderPass, EffectComposer, OutlinePass } from "three-outlinepass"
 import gsap from 'gsap';
 let testing = false;
 if (window.location.hash.substr(1).length && window.location.hash.substr(1) == 'test') {
@@ -24,7 +24,7 @@ function getThemeDir() {
     return myScript.src.replace(/themes\/(.*?)\/(.*)/g, 'themes/$1');
 }
 var themeDir = getThemeDir();
-const afcUrl = process.env.NODE_ENV == 'production' ? 'wp-json/acf/v3/options/acf-options-gallery' : 'http://localhost:8888/npht/wp-json/acf/v3/options/acf-options-gallery'
+const afcUrl = process.env.NODE_ENV == 'production' ? 'http://ducknest.co.uk/npht-gallery/wp-json/acf/v3/options/acf-options-gallery' : 'http://localhost:8888/npht/wp-json/acf/v3/options/acf-options-gallery'
 let database;
 const backButton = document.getElementById('goback')
 const pos = document.getElementById('pos')
@@ -47,6 +47,8 @@ let selectSpot = null
 let observing = false
 let mouseDown = false;
 var camSpeed = -1
+
+
 // sizes
 const sizes = {
     width: window.innerWidth,
@@ -99,6 +101,7 @@ const infoPointMaterial = new THREE.MeshBasicMaterial({ side: THREE.DoubleSide, 
 // CREATE OBJECTS ON LONG
 positions.forEach(element => {
     let infoPoint = new THREE.Mesh(infoPointGeometry, infoPointMaterial);
+
     if (i <= 5 || i > 10 && i < 16) {
         var newArtifact = new THREE.Mesh(wallMountedGeometry, material1);
         newArtifact.name = 'Wallmount-' + i
@@ -129,6 +132,7 @@ positions.forEach(element => {
     else {
         infoPoint.position.x = +30
     }
+    //    makeSprite(newArtifact.position.copy(element), newArtifact.name, newArtifact.position)
     infoPoints.push(infoPoint)
     artifacts.push(newArtifact)
     scene.add(newArtifact)
@@ -200,7 +204,6 @@ await axios.get(afcUrl)
     });
 
 
-
 // INFO WINDOW
 document.getElementById("infoClose").addEventListener("click", closeInfoWindow);
 function closeInfoWindow() {
@@ -247,8 +250,7 @@ function welcomeMessage() {
 function makeSprite(location, label, position) {
     var spritey = makeTextSprite(label, { borderColor: { r: 255, g: 255, b: 200, a: 1.0 }, fontsize: 30, backgroundColor: { r: 0, g: 0, b: 0, a: 1.0 }, textColor: { r: 255, g: 255, b: 255, a: 1.0 } });
     spritey.userData.id = location
-
-    spritey.visible = false
+    if (!testing) { spritey.visible = false }
     spritey.position.copy(position)
     if (location <= 10) {
         spritey.position.x -= 14
@@ -262,7 +264,7 @@ function makeLight(location, position) {
     const rectAreaLight = new THREE.RectAreaLight(0xffffff, 5, 5, 30)
     rectAreaLight.position.copy(position)
     rectAreaLight.position.y += 20
-    if (location < 10) {
+    if (location < 11) {
         rectAreaLight.rotation.y = -Math.PI / 2
         rectAreaLight.position.x -= 20
     } else {
@@ -275,35 +277,40 @@ function makeLight(location, position) {
 let models = []
 function loadModels(params) {
     database.forEach(async (element, i) => {
+        var back = i - 1
+
         // LOAD MODELS
         if (element.is_model) {
+            var location = parseInt(element.pedistal_location)
+
+            var selected = scene.getObjectByName('Pedistal-' + element.pedistal_location)
             const loadedArtifact = await loader.loadAsync(element['3d_model_']['url']);
             var boundingBox = new THREE.Box3().setFromObject(loadedArtifact.scene);
             let boundingBoxSize = boundingBox.getSize(new THREE.Vector3());
             const center = boundingBox.getCenter(new THREE.Vector3());
             let maxAxis = Math.max(boundingBoxSize.x, boundingBoxSize.y, boundingBoxSize.z);
             loadedArtifact.scene.scale.multiplyScalar(10 / maxAxis);
-            loadedArtifact.scene.position.copy(artifacts[element.pedistal_location].position)
-            loadedArtifact.scene.rotation.copy(artifacts[element.pedistal_location].rotation)
-            //   console.log(loadedArtifact.scene.localToWorld(new THREE.Vector3(0,0,0)))
-            if (artifacts[element.pedistal_location].userData.pedistal) {
-                loadedArtifact.scene.position.y += 12
-            }
+            loadedArtifact.scene.position.copy(selected.position)
+            loadedArtifact.scene.rotation.copy(selected.rotation)
+            loadedArtifact.scene.userData.location = element.pedistal_location
+            loadedArtifact.scene.userData.index = i
+            loadedArtifact.scene.position.y += 12
+
             models.push(loadedArtifact.scene)
             scene.add(loadedArtifact.scene)
-            makeLight(element.pedistal_location, loadedArtifact.scene.position)
-            makeSprite(element.pedistal_location, element.artifact_title, loadedArtifact.scene.position)
-            // console.log(loadedArtifact.scene.distanceTo(plane.position))
-            // TURN ON LIGHT AND LABEL
+
+            makeLight(location, selected.position)
+            makeSprite(parseInt(element.pedistal_location), element.artifact_title, loadedArtifact.scene.position)
         }
         // LOAD IMAGES
         else {
+            var selected = scene.getObjectByName('Wallmount-' + element.image_location)
             const colorTexture = textureLoader.load(element.image.url)
-            var cloned = artifacts[i].material.clone()
+            var cloned = artifacts[0].material.clone()
             cloned.map = colorTexture
-            artifacts[i].material = cloned
-            makeLight(element.pedistal_location, artifacts[element.image_location].position)
-            makeSprite(element.image_location, element.artifact_title, artifacts[element.image_location].position)
+            selected.material = cloned
+            makeLight(element.image_location, selected.position)
+            makeSprite(element.image_location, element.artifact_title, selected.position)
         }
         if (i == database.length - 1) {
             loading.style.display = 'none'
@@ -327,6 +334,11 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 //renderer.shadowMap.enabled = true;
 renderer.physicallyCorrectLights = true
+
+
+
+
+
 // CLICK ON OBJECTS
 function onDocumentMouseDown(event) {
     event.preventDefault();
@@ -339,34 +351,81 @@ function onDocumentMouseDown(event) {
     var intersects = raycaster.intersectObjects(sprites);
     if (intersects.length > 0 && !selectSpot) {
         for (var i = 0; intersects.length > 0 && i < intersects.length; i++) {
-            selectSpot = intersects[0].object.userData.id
+
+            selectSpot = parseInt(intersects[0].object.userData.id)
+           // console.log(selectSpot - 1)
             intersects[0].object.visible = false
         }
+        outlinePass.selectedObjects = []
     }
+
     else if (intersectsInfo.length > 0 && selectSpot) {
         openInfoWindow()
     }
 }
 
+
+
+// outline effetct
+var compose = new EffectComposer(renderer);
+var renderPass = new RenderPass(scene, camera);
+var outlinePass = new OutlinePass(new THREE.Vector2(window.innerWidth, window.innerHeight), scene, camera, models);
+outlinePass.renderToScreen = true;
+outlinePass.selectedObjects = '';
+compose.addPass(renderPass);
+compose.addPass(outlinePass);
+var params = {
+    edgeStrength: 1,
+    edgeGlow: 2,
+    edgeThickness: 0.5,
+    pulsePeriod: 3,
+
+};
+
+outlinePass.edgeStrength = params.edgeStrength;
+outlinePass.edgeGlow = params.edgeGlow;
+outlinePass.visibleEdgeColor.set(0xffffff);
+outlinePass.hiddenEdgeColor.set(0xffffff);
+
+compose.render(scene, camera)
+
+let hoverSpot = ''
+
 document.addEventListener('mousemove', onMouseMove, false);
 function onMouseMove(event) {
-    // calculate mouse position in normalized device coordinates
-    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-    mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
-    console.log('hek')
+    if (!intro && !selectSpot && !testing) {
+        // calculate mouse position in normalized device coordinates
+        mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+        mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
 
-    var intersects = raycaster.intersectObjects(sprites);
-    if (intersects.length > 0) {
-        document.body.style.cursor = "pointer";
-        for (var i = 0; intersects.length > 0 && i < intersects.length; i++) {
-            //   selectSpot = intersects[0].object.userData.id
-            intersects[i].object.material.color.set(0xff0000);
-           
 
+        var intersects = raycaster.intersectObjects(sprites);
+        if (intersects.length > 0) {
+            document.body.style.cursor = "pointer";
+            for (var i = 0; intersects.length > 0 && i < intersects.length; i++) {
+                var hoverSpot = intersects[0].object.userData.id-1
+                //console.log('hover',hoverSpot-1)
+                // console.log(hoverSpot)
+
+
+                models.forEach(element => {
+                    if (element.userData.location == hoverSpot) {
+                    
+                        outlinePass.selectedObjects = [models[hoverSpot]]
+                    }
+                })
+
+                intersects[i].object.material.color.set(0xff0000);
+        
+               // outlinePass.selectedObjects.push(artifacts[hoverSpot])
+                // // console.log(modelSelected)
+
+            }
+        } else {
+            document.body.style.cursor = "default";
+            sprites.forEach(i => i.material.color.set(0xffffff));
+            outlinePass.selectedObjects = []
         }
-    } else {
-        document.body.style.cursor = "default";
-        sprites.forEach(i => i.material.color.set(0xffffff));
     }
 }
 
@@ -377,7 +436,6 @@ const cameraControls = testing ? new OrbitControls(camera, renderer.domElement) 
 // listeners
 document.getElementById("goback").addEventListener("click", turnAround);
 document.addEventListener('mousedown', onDocumentMouseDown, false);
-
 document.getElementById("start").addEventListener("click", beginTour);
 
 function beginTour(params) {
@@ -416,6 +474,7 @@ const meshBBSize = cameraStand.geometry.boundingBox.getSize(new THREE.Vector3())
 const meshBBWidth = meshBBSize.x;
 const meshBBHeight = meshBBSize.y;
 const meshBBDepth = meshBBSize.z;
+
 function customFitTo() {
     const distanceToFit = cameraControls.getDistanceToFitBox(meshBBWidth, meshBBHeight, meshBBDepth);
     cameraControls.moveTo(
@@ -438,31 +497,42 @@ async function startTour() {
 }
 var nextPos = {}
 async function lookAtArtifact(params) {
-    nextPos.x = artifacts[selectSpot].position.x
-    nextPos.y = artifacts[selectSpot].position.y
-    nextPos.z = artifacts[selectSpot].position.z
-    // move camera up for pedistal
-    if (artifacts[selectSpot].userData.pedistal) {
-        nextPos.y += 10
+    if (selectSpot) {
+
+        console.log(selectSpot)
+        nextPos.x = artifacts[selectSpot - 1].position.x
+        nextPos.y = artifacts[selectSpot - 1].position.y
+        nextPos.z = artifacts[selectSpot - 1].position.z
+        // move camera up for pedistal
+        if (artifacts[selectSpot - 1].userData.pedistal) {
+            nextPos.y += 10
+        }
+        cameraControls.setTarget(nextPos.x, nextPos.y, nextPos.z, true)
+        if (selectSpot <= 9) {
+            cameraStand.position.set(nextPos.x - 30, nextPos.y, nextPos.z)
+            await cameraControls.setPosition(nextPos.x - 30, nextPos.y, nextPos.z, true)
+        } else {
+            cameraStand.position.set(nextPos.x + 30, nextPos.y, nextPos.z)
+            await cameraControls.setPosition(nextPos.x + 30, nextPos.y, nextPos.z, true)
+        }
+        cameraControls.saveState()
+        //  outlinePass.selectedObjects = infoPoints
+        backButton.style.display = 'block'
     }
-    cameraControls.setTarget(nextPos.x, nextPos.y, nextPos.z, true)
-    if (selectSpot <= 9) {
-        cameraStand.position.set(nextPos.x - 30, nextPos.y, nextPos.z)
-        await cameraControls.setPosition(nextPos.x - 30, nextPos.y, nextPos.z, true)
-    } else {
-        cameraStand.position.set(nextPos.x + 30, nextPos.y, nextPos.z)
-        await cameraControls.setPosition(nextPos.x + 30, nextPos.y, nextPos.z, true)
-    }
-    cameraControls.saveState()
-    backButton.style.display = 'block'
+
 }
+
+
 async function turnAround() {
     await cameraControls.setLookAt(cameraStand.position.x, 0, cameraStand.position.z, 0, 0, cameraStand.position.z, true)
-    console.log(sprites.find(x => x.userData.id == selectSpot).visible = true)
+    // console.log(sprites.find(x => x.userData.id == selectSpot).visible = true)
     selectSpot = null
     backButton.style.display = 'none'
 }
+
+
 const animate = () => {
+
     // INTRO - WALK INTO ROOM
     if (!testing) {
         if (intro && started) {
@@ -480,9 +550,13 @@ const animate = () => {
     const elapsed = clock.getElapsedTime();
     const updated = cameraControls.update(delta);
     requestAnimationFrame(animate);
+
     if (!testing) {
         customFitTo()
     }
-    return renderer.render(scene, camera);
+    return compose.render(scene, camera) && renderer.render(scene, camera);
+
 };
 animate()
+
+
